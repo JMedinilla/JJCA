e5fa8183c249868993b5653165bb68b37cdd5c4f
JMedinilla <javimedinilla@gmail.com>
2018-06-01 14:59:07 +0200

    Nuevas clases vacías y cambios al estilo

---
+++ app/src/main/java/ncatz/jvmed/jjca/ParteActivity.kt
@@ -0,0 +1,12 @@
+package ncatz.jvmed.jjca
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ParteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_parte)
+    }
+}
---
+++ app/src/main/java/ncatz/jvmed/jjca/VolumenActivity.kt
@@ -0,0 +1,12 @@
+package ncatz.jvmed.jjca
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class VolumenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_volumen)
+    }
+}
